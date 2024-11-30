69e00682b1c3a420244af123ba5ac4d8236e2496
mrfarhankhan007 <fk95034@gmail.com>
2024-11-30 10:18:27 +0530

    updated the code with more functions

--- test_math_func.py
+++ test_math_func.py
@@ -1,12 +1,34 @@
 import math_func
+import pytest
 
 
-def test_math_func_add():
+# @pytest.mark.number
+# @pytest.mark.skip(reason="")/skipif(condition,reason="")
+def test_add_integers():
     assert math_func.add(2, 2) == 4
-    assert math_func.add(4,4) == 8
+    assert math_func.add(4, 4) == 8
     assert math_func.add(5, 5) == 10
 
-def test_math_func_mul():
-    assert math_func.multiply(3,3) == 9
-    assert math_func.multiply(4,4) ==16
-    assert math_func.multiply(5,5) == 25
+
+# @pytest.mark.number
+def test_mul_integer():
+    assert math_func.multiply(3, 3) == 9
+    assert math_func.multiply(4, 4) == 16
+    assert math_func.multiply(5, 5) == 25
+
+
+# @pytest.mark.string
+def test_add_strings():
+    result = math_func.add("Hello", "World")
+    assert result == "HelloWorld"
+    assert type(result) is str
+    assert "heldio" not in result
+
+
+# @pytest.mark.string
+def test_mul_strings():
+    assert math_func.multiply("Hello ", 3) == "Hello Hello Hello "
+    result = math_func.multiply("Hello", 1)
+    assert result == "Hello"
+    assert type(result) is str
+    assert "Hello" in result
