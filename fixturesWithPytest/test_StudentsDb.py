f4e7f348c3831657c27d4963a8d926edbd98d27b
mrfarhankhan007 <fk95034@gmail.com>
2024-11-30 17:19:12 +0530

    -added a test using - FIXTURES WITH PYTEST - Did PY-tern Fixture in this commit -added a separate package for the same

--- fixturesWithPytest/test_StudentsDb.py
+++ fixturesWithPytest/test_StudentsDb.py
@@ -1,28 +1,25 @@
 import pytest
 from StudentDb import StudentDb
 
-db = None
 
-def setup_module(module):
-    global db
+@pytest.fixture(scope="module")
+def db():
     db = StudentDb()
     db.connect('fixturesWithPytest/data.json')
-
-
-def teardown_module(module):
+    yield db # yield because -> so that db.close() gets called in the end and act
+             # as a teardown method
     db.close()
 
 
-def test_scott_data():
+def test_scott_data(db):
     scott_data = db.get_data("Scott")
     assert scott_data["id"] == 1
     assert scott_data["name"] == "Scott"
     assert scott_data["result"] == "pass"
 
 
-def test_mark_data():
+def test_mark_data(db):
     mark_data = db.get_data("Mark")
     assert mark_data["id"] == 2
     assert mark_data["name"] == "Mark"
     assert mark_data["result"] == "fail"
-
